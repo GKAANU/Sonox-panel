e542ab37bd54a0edf8c6c02d3d48bd30b7d1677d
Kaan Uzun <90566093+GKAANU@users.noreply.github.com>
2025-01-15 02:47:50 +0300

    asdasd

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/socket.io/:path*',
+        destination: process.env.NEXT_PUBLIC_SOCKET_URL + '/socket.io/:path*',
+      },
+    ]
+  },
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
